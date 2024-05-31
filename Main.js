ae007409df88fe4456321832e839bafe5fd592d8
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-05-31 12:42:41 +0530

    first commit

---
+++ Main.js
@@ -0,0 +1,46 @@
+let NewsContainer = document.getElementById("NewsContainer")
+
+
+function Display(AllNews){
+
+    NewsContainer.innerHTML = "";
+    for(let News of AllNews)
+    {
+        let Img = News.urlToImage;
+        let Title = News.title;
+        let Desc = News.description;
+        let publishedAt = News.publishedAt;
+        let Author = News.author;
+        let LinkOfSourde = News.url;
+
+        NewsContainer.innerHTML += `
+        
+        <div class="NewsCard">
+            <img src=${Img} alt="">
+
+            <div class="NewsContent">
+                <h2>${Title}</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quidem.</p>
+                <p>${Author} . ${publishedAt}</p>
+                <a href = ${LinkOfSourde}>Read More</a>
+            </div>
+        </div>
+
+        `
+
+
+    }
+
+}
+
+
+async function GetNewsData(NewsTitle = "India")
+{
+    let Data = await fetch(`https://newsapi.org/v2/everything?q=${NewsTitle}&apiKey=20fd56da8c1247de82856d2fd80b1211`);
+    Data = await Data.json();
+
+    Display(Data.articles)
+    console.log(Data.articles)
+}
+
+GetNewsData();
